d8d46be97dfa87acd3e65775e2c0b1f920b5a388
SergeySverlov <sergei360@mail.ru>
2020-10-16 21:00:49 +0300

    Initial commit

---
+++ EnglishFlashCards/EnglishFlashCards/EnglishFlashCards/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using EnglishFlashCards.ViewModels;
+using EnglishFlashCards.Views;
+using Xamarin.Forms;
+
+namespace EnglishFlashCards
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ EnglishFlashCards/EnglishFlashCards/EnglishFlashCards/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using EnglishFlashCards.ViewModels;
+
+namespace EnglishFlashCards.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
